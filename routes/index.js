e5570d5218ceff5a5c7aad456ebe5395ec57bb5e
Rojar Smith <rojarsmith@gmail.com>
2020-09-16 13:18:21 +0800

    Add API: single-file and single-image.

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,28 @@
 var express = require('express');
 var router = express.Router();
-// var fs = require('fs');
 var multer = require('multer');
 
-console.log('aaa');
-var dddd = process.env.APP_IMAGE_STORAGE;
-console.log(dddd);
-
-var storage = multer.diskStorage({
+var storageFiles = multer.diskStorage({
   destination: function (req, file, cb) {
-    //cb(null, 'uploads/')
-    cb(null, process.env.REACT_APP_IMAGE_STORAGE)
-    //cb(null, 'E:\\static-service\\uploads')
+    cb(null, process.env.APP_FILES_STORAGE)
   },
-  // filename: function (req, file, cb) {
-  //   cb(null, file.filename + '.' + file.originalname.split('.')[file.originalname.split('.').length -1]) //Appending .jpg
-  // }
 });
 
-var upload = multer({
-  storage: storage
-  //dest: 'uploads/'
+var storageImages = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, process.env.APP_IMAGES_STORAGE)
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname + '-' + Date.now() + '.' + file.originalname.split('.')[file.originalname.split('.').length - 1]) //Appending .jpg
+  }
+});
+
+var uploadFiles = multer({
+  storage: storageFiles
+});
+
+var uploadImages = multer({
+  storage: storageImages
 });
 
 /* GET home page. */
@@ -28,19 +30,26 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/notmultiple', upload.single('file'), async (req, res) => {
+router.post('/single-file', uploadFiles.single('file'), async (req, res) => {
   var file = req.file;
-  console.log('  File uploaded');
+  logFile('File', file);
+  res.send();
+});
+
+router.post('/single-image', uploadImages.single('file'), async (req, res) => {
+  var file = req.file;
+  logFile('Image', file);
+  res.send();
+});
+
+function logFile(type, file) {
+  console.log('  ' + type + ' uploaded');
   console.log('    Path: %s', file.filename);
   console.log('    Type: %s', file.mimetype);
   console.log('    Name: %s', file.originalname);
   console.log('    Size: %s', file.size);
   console.log('    Path: %s', file.path);
   console.log('    Field: %s', file.fieldname);
-  // fs.rename(file.path, file.path + '.' + file.originalname.split('.')[file.originalname.split('.').length - 1], function (err) {
-  //   if (err) console.log('ERROR: ' + err);
-  // });
-  res.send();
-});
+}
 
 module.exports = router;
