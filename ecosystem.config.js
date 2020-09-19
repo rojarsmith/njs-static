eadeaa4975e40b9e7cf1b601df2de1ec1127f2a6
Rojar Smith <rojarsmith@gmail.com>
2020-09-19 10:42:42 +0800

    Add ecosystem.config.js for PM2.

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps: [
+    // First application
+    {
+      name: 'njs-static',
+      script: 'app.js',
+      cwd: '~/service/static/njs-static',
+      exec_mode: 'cluster',
+      instance: 0,
+      watch: false,
+      max_memory_restart: '200M',
+      time: true,
+      log_date_format: 'YYYY-MM-DD HH:mm Z',
+      error_file: '~/service/log',
+      out_file: '~/service/log',
+      autorestart: true,
+      env: {
+        COMMON_VARIABLE: "true"
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ]
+};
